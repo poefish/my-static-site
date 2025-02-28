feb7009f751633766e7f0568532a984faf1515d0
unknown <2189329811@qq.com>
2025-02-28 13:27:06 +0800

    更新了手机适配操作

--- scripts/listener.js
+++ scripts/listener.js
@@ -1,26 +1,45 @@
-function Listener({ move: moveFn, start: startFn }) {
+function Listener({move,start}) {
     window.addEventListener('keyup', function(e) {
       switch (e.code) {
         case 'ArrowUp':
-          moveFn({ row: -1, column: 0 });
+          move({ row: -1, column: 0 });
           break;
         case 'ArrowLeft':
-          moveFn({ row: 0, column: -1 });
+          move({ row: 0, column: -1 });
           break;
         case 'ArrowRight':
-          moveFn({ row: 0, column: 1 });
+          move({ row: 0, column: 1 });
           break;
         case 'ArrowDown':
-          moveFn({ row: 1, column: 0 });
+          move({ row: 1, column: 0 });
           break;
       }
     });
   
-    const buttons = document.querySelectorAll('button');
-    for (let i = 0; i < buttons.length; i++) {
-      buttons[i].addEventListener('click', function() {
-        startFn();
-      });
-    }
-  }
+       // 处理 again 按钮点击事件
+       const buttons = document.getElementsByClassName('again');
+       for (let i = 0; i < buttons.length; i++) {
+            buttons[i].addEventListener('click', function() {
+            start();
+            });
+          }
+   
+   // 处理触摸事件
+   const upButton = document.getElementById('up-button');
+   const downButton = document.getElementById('down-button');
+   const leftButton = document.getElementById('left-button');
+   const rightButton = document.getElementById('right-button');
+
+   if (upButton && downButton && leftButton && rightButton) {
+       upButton.addEventListener('touchstart', () => move({ row: -1, column: 0 }));
+       downButton.addEventListener('touchstart', () => move({ row: 1, column: 0 }));
+       leftButton.addEventListener('touchstart', () => move({ row: 0, column: -1 }));
+       rightButton.addEventListener('touchstart', () => move({ row: 0, column: 1 }));
+   }
+}
+
+
+
   
+
+
